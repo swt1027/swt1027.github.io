32d6d51852e6825f5df2c33834a9a2010d067337
swt1027 <swt1027@live.missouristate.edu>
2017-10-29 12:58:24 -0500

    Date and Validation

---
+++ customerValidation.js
@@ -0,0 +1,31 @@
+// JavaScript source code
+//validate user information
+
+function validate() {
+    var fName = document.getElementById("fistname");
+    var lName = document.getElementById("lastname");
+    var mail = document.getElementById("email");
+    var msg = document.getElementById("message");
+
+    //check if values are empty
+    if (fName.value == "") {
+        alert ("Please enter your first name.");
+        return false;
+    }
+    if (lName.value == "") {
+        alert ("Please enter your last name.");
+        return false;
+    }
+    if (mail.value == "") {
+        alert ("Please enter your email.");
+        return false;
+    }
+    if (msg == "") {
+        alert ("Please enter a message.");
+        return false;
+    } else
+      return true;
+}
+//register event handlers
+document.getElementById("customerForm").onsubmit = validate;
+
---
+++ date.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+//Get today's date
+
+//get the current date
+var today = new Date();
+
+var dateString = today.toLocaleString();
+
+//display
+document.write(
+    "Today is: " + dateString);
+
