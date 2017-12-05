876e556eb05798dd6f6e74e889f11e645d8f6c7e
swt1027 <swt1027@live.missouristate.edu>
2017-12-05 15:23:46 -0600

    race

--- date.js
+++ date.js
@@ -1,5 +1,7 @@
 // JavaScript source code
-
+function init() {
+    reset();
+}
 
 
 /* This line gets all the HTML elements with class="light" and assigns them to a variable called LIGHTS. */
@@ -33,34 +35,39 @@ function ready() {
     imgObj2 = document.getElementById('yellowcar');
     imgObj1.style.position = 'absoulte';
     imgObj1.style.top = '0px';
-    imgObj1.style.left = '100px';
+    imgObj1.style.left = '-100px';
     imgObj2.style.position = 'absolute';
     imgObj2.style.top = '100px';
-    imgObj2.style.left = '100px';
+    imgObj2.style.left = '-100px';
 
     green();
 }
 
 function green() {
-    var greenSpeed = Math.round((Math.random() * 2) + 50);
-    var yellowSpeed = Math.round((Math.random() * 2) + 50);
+    
+
+    var greenSpeed = Math.round((Math.random() * 2) + 35);
+    var yellowSpeed = Math.round((Math.random() * 2) + 35);
     left = parseInt(imgObj1.style.left, 7);
     left2 = parseInt(imgObj2.style.left, 7);
 
-    if (500 >= left && 500 >= left2) {
-        imgObj1.style.left = left + greenSpeed + 'px';
-        imgObj2.style.left = left + yellowSpeed + 'px';
-        animate = setTimeout(green, 200);
+    if (900 >= left && 900 >= left2) {
+        imgObj1.style.left = (left + greenSpeed) + 'px';
+        imgObj1.style.visibility = 'visible';
+        imgObj2.style.left = (left + yellowSpeed) + 'px';
+        imgObj2.style.visibility = 'visible';
+
+        animate = setTimeout(function () { green(); }, 300);
     }
 
     else if (imgObj1.style.left > imgObj2.style.left) {
-        alert("Green Car Wins!");
+        alert("Green Car Wins! Click Red Light to Reset");
     }
     else if (imgObj2.style.left > imgObj1.style.left) {
-        alert("Yellow Car Wins!");
+        alert("Yellow Car Wins! Click Red Light to Reset");
     }
     else if (imObj1.style.left == imgObj2.style.left) {
-        alert("Both Cars Tie!");
+        alert("Both Cars Tie! Click Red to Reset");
     }
 
 
@@ -76,9 +83,11 @@ function reset() {
     imgObj2 = document.getElementById('yellowcar');
     imgObj1.style.position = 'absoulte';
     imgObj1.style.top = '0px';
-    imgObj1.style.left = '100px';
+    imgObj1.style.left = '-300px';
     imgObj2.style.position = 'absolute';
     imgObj2.style.top = '100px';
-    imgObj2.style.left = '100px';
+    imgObj2.style.left = '400px';
 }
+
+window.onload = function () { init(); };
 
